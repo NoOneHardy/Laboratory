956ea7c5990ce7f0bd440c2a6facf0d0f31c7cac
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-09-05 22:48:27 +0200

    feature: upload files (#7)

---
+++ Java/cdn/service/src/main/java/ch/no1hardy/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package ch.no1hardy;
+
+public class StorageFileNotFoundException extends RuntimeException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
